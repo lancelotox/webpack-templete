d55a7c91c13b8731e5b04c0d3b5f8fc7cdc0cb13
周峰 <18226814491@163.com>
2023-08-04 17:51:15 +0800

    进度条

--- webpack.js
+++ webpack.js
@@ -21,7 +21,6 @@ const webpack = require('webpack');
 const devServer = require('webpack-dev-server');
 // 导入配置
 const Options = require('./webpack.config.js').getOptions(ScriptParams);
-
 const package = require('./package.json');
 
 // 控制台进度条
@@ -31,6 +30,34 @@ let progressPlugin = new WebpackBar({
   color: "#2f90b9",  // 默认green，进度条颜色支持HEX
   basic: true,   // 默认true，启用一个简单的日志报告器
   profile: false,  // 默认false，启用探查器。
+  reporter: {
+    start(context) {
+      // 在（重新）编译开始时调用
+      // const { start, progress, message, details, request, hasErrors } = context
+      console.clear();
+    },
+    change(context) {
+      // 在 watch 模式下文件更改时调用
+    },
+    update(context) {
+      // 在每次进度更新后调用
+    },
+    done(context) {
+      // 编译完成时调用
+    },
+    progress(context) {
+      // 构建进度更新时调用
+    },
+    allDone(context) {
+      // 当编译完成时调用
+    },
+    beforeAllDone(context) {
+      // 当编译完成前调用
+    },
+    afterAllDone(context) {
+      // 当编译完成后调用
+    }
+  }
 })
 if (!Array.isArray(Options.plugins)) Options.plugins = [];
 Options.plugins.push(progressPlugin);
